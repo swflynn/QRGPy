d9ac951d798a4a7395dc157ed3955212d09103dd
swflynn <swflynn@uci.edu>
2020-08-14 19:51:52 -0700

    Skeleton Functions to start the project

---
+++ qrg/main.py
@@ -0,0 +1,31 @@
+#=================20==================40==================60==================80
+#                                   QRGPy
+#==============================================================================#
+#Generate a Quasi-Regular Grid for any d-dimensional system of interest.
+#==============================================================================#
+def P_i():
+#Target distribution function
+#In general and d-dimensional system should be allowed
+    pass
+
+def box_size_P():
+#compute the box for normalizing P
+    pass
+
+def Integral_P():
+#Compute the first moment of the distribution to normalize the distribution
+    pass
+
+def Initial_Grid():
+#Use rejection to generate an initial set of points according to P
+    pass
+
+def Pseudo_Pairwise_Energy():
+#Define the Pseudo-Potential to minimize when generating the QRG
+#In general this could be user defined
+    pass
+
+
+def QR_Minimize():
+#Use the QR formalism to generate a QRG from the initial grid
+    pass

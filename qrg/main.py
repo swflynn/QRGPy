c28a43b5378885214b8d263d9dcde7080c511da4
swflynn <swflynn@uci.edu>
2020-08-16 16:36:19 -0700

    Add potential example

--- qrg/main.py
+++ qrg/main.py
@@ -6,23 +6,32 @@
 #==============================================================================#
 import numpy as np
 #==============================================================================#
-def P_i(d,x_i,V_i):
+def P_i(d,E_cut,x_i,integral_P):
 #Target distribution function
 #In general and d-dimensional system should be allowed, begin by defining sys.
-    pass
-
+#==============================================================================#
+#E_cut              ==>Distribution cutoff contour
+#x_i(d)             ==>i-th grid points coordinates (x^i=x_1,x_2,...x_d)
+#V_i                ==>Potential Energy evaluation V(x_i)
+#E_cut              ==>Distribution cutoff contour
+#integral_P         ==>Normalization constant for the distribtion P(x)
+#P_i                ==>evaluate P(x_i)
+#==============================================================================#
+    V_i=Morse(omegas,D_morse,x_i)
+    if(V_i<E_cut): P=(E_cut-V_i)**(d/2.)/integral_P
+    else: P=1e-20                           #Define distribution=0 beyond Ecut
+    return P
+#==============================================================================#
 def Morse(omegas,D_morse,x_i):
+#==============================================================================#
 #Potential Energy: Sum of 1D Morse potentials
 #==============================================================================#
-#d                ==>Coordinate dimensionality (x^i=x_1,x_2,...x_d)
+#omegas(d)        ==>Parameter for Morse Potential
 #x_i(d)           ==>i-th grid points coordinates (x^i=x_1,x_2,...x_d)
 #D_morse          ==>Parameter for Morse Potential
-#omegas(d)        ==>Parameter for Morse Potential
-#V_i              ==>Potential Energy evaluation V(x_i)
 #==============================================================================#
     return D_morse*np.sum((np.exp(-omegas*x_i)-1.)**2 )
-
-
+#==============================================================================#
 def box_size_P():
 #compute the box for normalizing P
     pass
@@ -48,15 +57,20 @@ def QR_Minimize():
 
 
 #==============================================================================#
+d=2
 D_morse=12.
+E_cut=100.
+integral_P=1.
+#==============================================================================#
 omega_list=list()
 omega_list.append(3.)
 omega_list.append(4.)
 omegas=np.array(omega_list)
-
+#==============================================================================#
 x_list=list()
 x_list.append(1.)
 x_list.append(2.)
 x_i=np.array(x_list)
-
+#==============================================================================#
 print(Morse(omegas,D_morse,x_i))
+print(P_i(d,E_cut,x_i,integral_P))
